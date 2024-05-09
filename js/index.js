707f64be8c71ed11831fba1f45b9cf3a42707c1f
Max <bilousmaks27@gmail.com>
2024-05-09 20:24:19 +0300

    smoothness

--- js/index.js
+++ js/index.js
@@ -12,3 +12,31 @@ function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Отримуємо всі елементи розділів, які мають бути анімовані
+const sections = document.querySelectorAll('.section1-content, .section2-content, .Contact-content');
+
+// Функція для перевірки, чи елемент перевищив середину вікна
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top <= (window.innerHeight / 1.2) &&
+        rect.bottom >= (window.innerHeight / 2)
+    );
+}
+
+// Функція, яка встановлює клас .visible для розділів, які перевищили середину вікна
+function toggleVisibility() {
+    sections.forEach(section => {
+        if (isElementInViewport(section)) {
+            section.classList.add('visible');
+        } else {
+            section.classList.remove('visible');
+        }
+    });
+}
+
+// Викликаємо функцію при завантаженні сторінки та при прокрутці
+window.addEventListener('load', toggleVisibility);
+window.addEventListener('scroll', toggleVisibility);
+
