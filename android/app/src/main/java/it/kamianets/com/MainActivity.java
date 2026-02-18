b6cc677f485d481fd9d3d841c65c22335d6740ad
Honchar Denys <crackeraki@gmail.com>
2026-02-18 11:05:26 +0200

    init android app

---
+++ android/app/src/main/java/it/kamianets/com/MainActivity.java
@@ -0,0 +1,5 @@
+package it.kamianets.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'it.kamianets.com',
+	appName: 'IT Kamianets',
+	webDir: 'dist/theme-tailwind/browser',
+};
+
+export default config;
