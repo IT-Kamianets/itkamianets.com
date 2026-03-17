57d8626aed7b781f56357c0b121450c9a9b5af0e
Honchar Denys <crackeraki@gmail.com>
2026-03-17 10:38:35 +0200

    feat: restructure routing and layout components for manage and guest sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { authenticatedGuard } from './feature/user/authenticated.guard';
 
 export const routes: Routes = [
 	{
-		path: 'sign',
-		loadComponent: () =>
-			import('./pages/sign/sign.component').then((m) => m.SignComponent),
-	},
-	{
 		path: '',
 		loadComponent: () =>
 			import('./layouts/public/public.component').then((m) => m.PublicComponent),
@@ -16,14 +11,6 @@ export const routes: Routes = [
 				path: '',
 				loadComponent: () =>
 					import('./pages/home/home.component').then((m) => m.HomeComponent),
-			},
-			{
-				path: 'manage/profile',
-				canActivate: [authenticatedGuard],
-				loadComponent: () =>
-					import('./pages/manage/profile/profile.component').then(
-						(m) => m.ProfileComponent,
-					),
 			},
 			{
 				path: 'projects',
@@ -64,6 +51,117 @@ export const routes: Routes = [
 				loadComponent: () =>
 					import('./pages/proposals/proposals.component').then(
 						(m) => m.ProposalsComponent,
+					),
+			},
+		],
+	},
+	{
+		path: '',
+		loadComponent: () => import('./layouts/guest.component').then((m) => m.GuestComponent),
+		children: [
+			{
+				path: 'sign',
+				loadComponent: () =>
+					import('./pages/sign/sign.component').then((m) => m.SignComponent),
+			},
+		],
+	},
+	{
+		path: 'manage',
+		loadComponent: () => import('./layouts/manage.component').then((m) => m.ManageComponent),
+		children: [
+			{
+				path: '',
+				pathMatch: 'full',
+				redirectTo: 'profile',
+			},
+			{
+				path: 'profile',
+				canActivate: [authenticatedGuard],
+				loadComponent: () =>
+					import('./pages/manage/profile/profile.component').then(
+						(m) => m.ProfileComponent,
+					),
+			},
+			{
+				path: 'merch',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Merch' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'orders',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Orders' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'companies',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Companies' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'projects',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Projects' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'articles',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Articles' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'schools',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Schools' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'jobs',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Jobs' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'events',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Events' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
+					),
+			},
+			{
+				path: 'competitions',
+				canActivate: [authenticatedGuard],
+				data: { title: 'Competitions' },
+				loadComponent: () =>
+					import('./pages/manage/section/section.component').then(
+						(m) => m.SectionComponent,
 					),
 			},
 		],
