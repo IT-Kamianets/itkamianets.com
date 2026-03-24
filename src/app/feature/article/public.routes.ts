ef0ae26a1de5d6b5fdb65775e2690e621639359e
Bogdan <kn1b22.starenkyi@kpnu.edu.ua>
2026-03-24 10:37:56 +0200

    feat(article): implement articles component with routing and form functionality

--- src/app/feature/article/public.routes.ts
+++ src/app/feature/article/public.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+                path: 'articles',
+                loadComponent: () =>
+                    import('./pages/articles/articles.component').then(
+                        (m) => m.ArticlesComponent,
+                    ),
+            }
+        ];
