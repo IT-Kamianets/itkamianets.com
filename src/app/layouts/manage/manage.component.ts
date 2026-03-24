08b23e4f4278bc0b70cb842f6f6ad73c57144265
Vadim <kn1b22.valtser@kpnu.edu.ua>
2026-03-24 09:46:08 +0200

    refactor: update styles and structure for project management components
    - Refactored SCSS for project management components to improve layout and responsiveness.
    - Introduced a new top bar for project management with actions and headings.
    - Updated button styles for better interaction feedback.
    - Enhanced dialog styles for project creation and editing.
    - Adjusted table styles for publications to improve readability.
    - Implemented a new service structure for project API interactions, simplifying HTTP requests.
    - Added new navigation item for "Profiles" in the manage component.
    - Improved overall layout and overflow handling in manage sidebar.

--- src/app/layouts/manage/manage.component.ts
+++ src/app/layouts/manage/manage.component.ts
@@ -18,12 +18,12 @@ import { ManageHeaderComponent } from '../manage-header/manage-header.component'
 })
 export class ManageComponent {
 	protected readonly navItems = [
+		{ label: 'Profiles', path: '/manage/profile', icon: 'account_circle' },
 		{ label: 'Merch', path: '/manage/merch', icon: 'shopping_bag' },
 		{ label: 'Orders', path: '/manage/orders', icon: 'receipt_long' },
 		{ label: 'Companies', path: '/manage/companies', icon: 'apartment' },
 		{ label: 'Projects', path: '/manage/projects', icon: 'deployed_code' },
 		{ label: 'Articles', path: '/manage/articles', icon: 'article' },
-		{ label: 'Profiles', path: '/manage/profile', icon: 'account_circle' },
 		{ label: 'Schools', path: '/manage/schools', icon: 'school' },
 		{ label: 'Jobs', path: '/manage/jobs', icon: 'work' },
 		{ label: 'Events', path: '/manage/events', icon: 'event' },
