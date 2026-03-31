b207f061cc31b2b077d53e456d2750f680454de1
Makarenko <makar@example.com>
2026-03-31 09:02:59 +0300

    feat(review): add reviews section to management navigation

--- src/app/layouts/manage/manage.component.ts
+++ src/app/layouts/manage/manage.component.ts
@@ -29,5 +29,6 @@ export class ManageComponent {
 		{ label: 'Events', path: '/manage/events', icon: 'event' },
 		{ label: 'Competitions', path: '/manage/competitions', icon: 'emoji_events' },
 		{ label: 'People', path: '/manage/people', icon: 'group' },
+		{ label: 'reviews', path: '/manage/reviews', icon: 'group' },
 	];
 }
